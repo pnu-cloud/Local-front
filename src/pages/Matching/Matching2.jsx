d1c9ca348b672521919927e2441894dae7bcf655
redzzzi <myqueen1346@naver.com>
2024-08-24 15:05:51 +0900

    feature : 게시글 및 pagination 구현

--- src/pages/Matching/Matching2.jsx
+++ src/pages/Matching/Matching2.jsx
@@ -3,11 +3,9 @@ import { Box } from '@mui/material';
 import BannerSection from '../../components/Section/BannerSection';
 import ListSection from '../../components/Section/ListSection';
 import { posts } from '../../constants/boardInfo';
-// import { useParams } from 'react-router-dom';
 import GangwonMap from '../../components/Maps/GangwonMap';
 
 const Matching2 = () => {
-  // const { region } = useParams();
   return (
     <>
       <BannerSection step="2" question="Q2. 지역이 선택되었어요! 원하는 숙소의 위치가 있나요?" />

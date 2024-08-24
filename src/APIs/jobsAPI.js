c341126699a3b7228586ba0e09be33e98d4a0b0c
myeowon <myw7931@gmail.com>
2024-08-25 04:29:55 +0900

    matching 3 전체

--- src/APIs/jobsAPI.js
+++ src/APIs/jobsAPI.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const jobsAPI = async (local) => {
+const JobsAPI = async (local) => {
   try {
     const response = await axios.get(`https://life.pnu.app/matching/jobs/distance/${local}`);
+    console.log(response.data);
     return response.data; // 데이터를 그대로 반환
   } catch (error) {
     console.error('Error fetching dormitories:', error);
     throw error; // 필요하다면 에러를 호출한 쪽으로 전달
   }
 };
-export default jobsAPI;
+export default JobsAPI;
